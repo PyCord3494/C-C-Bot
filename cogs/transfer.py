a8386d7b6c56a4ddd9aa814366a21961f8f6acd6
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-30 22:11:29 -0500

    Embeds fixing, including colors!

--- cogs/transfer.py
+++ cogs/transfer.py
@@ -16,27 +16,27 @@ class Transfer(commands.Cog):
 		author = ctx.author
 		currency = self.bot.get_cog("Economy").getCurrency()
 		if user is None: # if user is not found
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description="Invalid member.")
+			embed = discord.Embed(title="C&C Bot: Send", color=0xff0000, description="Invalid member.")
 			embed.set_thumbnail(url=ctx.author.avatar_url)
 			await ctx.send(embed=embed)
 			return
 		if await self.bot.get_cog("Economy").accCheck(author.id) == False: # accCheck for user sending money
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description="You must $start your account before you can use my commands.")
+			embed = discord.Embed(title="C&C Bot: Send", color=0xff0000, description="You must $start your account before you can use my commands.")
 			embed.set_thumbnail(url=ctx.author.avatar_url)
 			await ctx.send(embed=embed)
 			return
 		if await self.bot.get_cog("Economy").accCheck(user.id) == False: # accCheck for user receiving the money
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description=f"{user.mention} must $start his account before you can send him any money.")
+			embed = discord.Embed(title="C&C Bot: Send", color=0xff0000, description=f"{user.mention} must $start his account before you can send him any money.")
 			embed.set_thumbnail(url=user.avatar_url)
 			await ctx.send(embed=embed)
 			return
 		if author == user: # if user is attempting to send money to himself
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description=f"You can't send {currency} to yourself.")
+			embed = discord.Embed(title="C&C Bot: Send", color=0xff0000, description=f"You can't send {currency} to yourself.")
 			embed.set_thumbnail(url=ctx.author.avatar_url)
 			await ctx.send(embed=embed)
 			return
 		if amnt < 10: # if user is attempting to send less than 10
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description=f"You need to transfer at least 10. {currency}")
+			embed = discord.Embed(title="C&C Bot: Send", color=0xff0000, description=f"You need to transfer at least 10 {currency}")
 			await ctx.send(embed=embed)
 			return
 
@@ -46,7 +46,7 @@ class Transfer(commands.Cog):
 			embed = discord.Embed(title="C&C Bot: Send", color=0xdfe324, description=f"{author.mention} has sent {amnt} {currency}, and after taxes, {user.mention} has received {int(amnt * 0.92)} {currency}.")
 			embed.set_thumbnail(url=user.avatar_url)
 		else:
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description=f"You do not have enough {currency} to send that much.")
+			embed = discord.Embed(title="C&C Bot: Send", color=0xff0000, description=f"You do not have enough {currency} to send that much.")
 			embed.set_thumbnail(url=ctx.author.avatar_url)
 
 		await ctx.send(embed=embed)
