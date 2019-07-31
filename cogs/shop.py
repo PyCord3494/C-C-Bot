0643beaa6d49b4a009b34dff2c2d8ceabad13609
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-30 22:12:02 -0500

    More embeds!

--- cogs/shop.py
+++ cogs/shop.py
@@ -12,45 +12,48 @@ class Shop(commands.Cog):
 	async def shop(self, ctx):
 		if ctx.invoked_subcommand is None:
 			if await self.bot.get_cog("Economy").accCheck(ctx.author.id) == False:
-				embed = discord.Embed(title="C&C Bot: Shop", color=0xfd0006, description="You must $start your account before you can use my commands.")
+				embed = discord.Embed(title="C&C Bot: Shop", color=0xff0000, description="You must $start your account before you can use my commands.")
 				await ctx.send(embed=embed)
 				return
 
-			await ctx.send("```ML\nID            ITEMS                COST\n" # the shop menu
-								+ "1   not seperated custom role!  200,000\n"
-								+ "2     seperated custom role!    500,000\n"
-								+ "----------------------------------------\n"
-								+ "Use +shop buy <id>\n```")
+			embed = discord.Embed(title="C&C Bot: Shop", color=0xdfe324, description="ID            ITEMS                COST\n1   not seperated custom role!  200,000\n2     seperated custom role!    500,000\n----------------------------------------\nUse +shop buy <id>")
+			await ctx.send(embed=embed)
+			# await ctx.send("```ML\nID            ITEMS                COST\n" # the shop menu
+			# 					+ "1   not seperated custom role!  200,000\n"
+			# 					+ "2     seperated custom role!    500,000\n"
+			# 					+ "----------------------------------------\n"
+			# 					+ "Use +shop buy <id>\n```")
 
 	@shop.command()
 	async def buy(self, ctx, ID: int):
 		currency = self.bot.get_cog("Economy").getCurrency()
 		discordId = ctx.message.author.id
 		if await self.bot.get_cog("Economy").accCheck(discordId) == False:
-			embed = discord.Embed(title="C&C Bot: Shop", color=0xfd0006, description="You must $start your account before you can use my commands.")
+			embed = discord.Embed(title="C&C Bot: Shop", color=0xff0000, description="You must $start your account before you can use my commands.")
 			await ctx.send(embed=embed)
 			return
 		if ID != 1 and ID != 2: # if ID isn't valid
-			embed = discord.Embed(title="C&C Bot: Shop", color=0xfd0006, description="Invalid item ID.")
+			embed = discord.Embed(title="C&C Bot: Shop", color=0xff0000, description="Invalid item ID.")
 			await ctx.send(embed=embed)
 			return
 		
 		bal = self.bot.get_cog("Economy").getBal(discordId) # grabs balance
 		price = self.items[ID - 1] # grabs price from array in __init__
 		if bal < price: # if cost is too much
-			embed = discord.Embed(title="C&C Bot: Send", color=0xfd0006, description=f"That will cost you {price} {currency}, but you only have {bal} {currency}")
+			embed = discord.Embed(title="C&C Bot: Shop", color=0xff0000, description=f"That will cost you {price} {currency}, but you only have {bal} {currency}")
 			await ctx.send(embed=embed)
 			return
 
 		await self.bot.get_cog("Economy").editBal(discordId, -price) # subtract price from bal
 		
+		embed = discord.Embed(title="C&C Bot: Shop", color=0xdfe324, description="Purchase successful! I sent a message to <@435806214484393996> and he will be messaging you when he can. Please be patient. :)")
+		await ctx.send(embed=embed)
+
 		if ID == 1: # shop item #1
-			guildOwner = await self.bot.fetch_user(547475078082985990)
-			await guildOwner.send(f"User {ctx.author.mention} ({ctx.author.id}) has purchased \"Not separated Custom Role!\" for {price} {currency}") 
+			await ctx.guild.owner.send(f"User {ctx.author.mention} ({ctx.author.id}) has purchased \"Not separated Custom Role!\" for {price} {currency}") 
 		
 		elif ID == 2: # shop item #2
-			guildOwner = await self.bot.fetch_user(547475078082985990)
-			await guildOwner.send(f"User {ctx.author.mention} ({ctx.author.id}) has purchased \"Seperated Custom Role!\" for {price} {currency}") 
+			await ctx.guild.owner.send(f"User {ctx.author.mention} ({ctx.author.id}) has purchased \"Seperated Custom Role!\" for {price} {currency}") 
 
 def setup(bot):
 	bot.add_cog(Shop(bot))
