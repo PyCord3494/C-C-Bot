5940e451258b3e2f7d96d218f73839f75eadfa22
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-30 22:12:20 -0500

    More embeds!

--- cogs/rewards.py
+++ cogs/rewards.py
@@ -1,6 +1,4 @@
-# Cog for all the transactions/logging 
-# Aka anything dealing with file editing
-
+# Cog for anything related to free money and rewards
 
 import discord
 from discord.ext import commands
@@ -30,9 +28,10 @@ class Rewards(commands.Cog):
 
 			await self.bot.get_cog("Economy").editBal(discordId, reward)
 			bal = self.bot.get_cog("Economy").getBal(discordId)
-			await ctx.send(f"You now have {bal} {currency}.")
+			embed = discord.Embed(title="C&C Bot: Rewards", color=0xdfe324, description=f"{reward} {currency} has been added to your account. You now have {bal} {currency}")
+			await ctx.send(embed=embed)
 		else: 
-			embed = discord.Embed(title="C&C Bot: Rewards", color=0xfd0006, description="You must $start your account before you can use my commands.")
+			embed = discord.Embed(title="C&C Bot: Rewards", color=0xff0000, description="You must $start your account before you can use my commands.")
 			await ctx.send(embed=embed)
 
 		
